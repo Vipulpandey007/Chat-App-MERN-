edc86cca74a64e484854c3b553650fa2baab7757
Vipulpandey007 <vipulpandey708@gmail.com>
2024-02-18 14:02:33 +0530

    empty and active message addes

--- Chat-app/src/components/Chat.jsx
+++ Chat-app/src/components/Chat.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
 import Logo from "./Logo";
+import { UserContext } from "../context/UserContext";
+import EmptyMessage from "./EmptyMessage";
+import Message from "./Message";
 
 const Chat = () => {
   const [ws, setWs] = useState(null);
   const [selectedPeople, setSelectedPeople] = useState(null);
   const [online, setOnline] = useState({});
+  const { id } = useContext(UserContext);
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4000");
     setWs(ws);
@@ -28,50 +32,33 @@ const Chat = () => {
     }
   };
 
+  const onlinePeople = { ...online };
+  delete onlinePeople[id];
   return (
     <div className="flex h-screen">
       <div className="bg-white w-1/3 ">
         <Logo />
-        {Object.keys(online).map((userId, i) => (
+
+        {Object.keys(onlinePeople).map((userId, i) => (
           <div
             onClick={() => setSelectedPeople(userId)}
             className={
-              "border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer " +
+              "border-b border-gray-100  flex items-center gap-2 cursor-pointer " +
               (userId === selectedPeople ? "bg-blue-50" : "")
             }
             key={i}
           >
-            <Avatar email={online[userId]} userId={userId} />
-            <span className="text-gray-700">{online[userId]}</span>
+            {userId === selectedPeople && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2 py-2 pl-4 items-center">
+              <Avatar email={online[userId]} userId={userId} />
+              <span className="text-gray-700">{online[userId]}</span>
+            </div>
           </div>
         ))}
       </div>
-      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-        <div className="flex-grow">Messages</div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Type something..."
-            className="bg-white border p-2 flex-grow rounded-lg"
-          />
-          <button className="bg-gray-500 p-2 text-white rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+      {!selectedPeople ? <EmptyMessage /> : <Message />}
     </div>
   );
 };
